9b8c14cd4aa9df7ab9c522a4a11d08a14976ce4d
Nikita Akimov <korchiy@yandex.ru>
2019-09-28 22:06:47 +0300

    update

---
+++ transform.py
@@ -0,0 +1,20 @@
+# Nikita Akimov
+# interplanety@interplanety.org
+#
+# GitHub
+#   https://github.com/Korchy/b3d_lib_int
+
+
+import math
+from mathutils import Matrix
+
+
+class TransformMesh:
+
+    @staticmethod
+    def rotate_object_local(obj, axis, angle, angle_unit='radians'):
+        # rotate object around axis on angle
+        if axis in ('X', 'Y', 'Z') and angle_unit in ('radians', 'degrees'):
+            if angle_unit == 'degrees':
+                angle = math.radians(angle)
+            obj.rotation_euler = (obj.rotation_euler.to_matrix() @ Matrix.Rotation(angle, 3, axis)).to_euler()
